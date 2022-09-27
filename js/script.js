381e64b074ce268787917868e245ebc697ed7c33
allison_c <anconrey@gmail.com>
2022-09-26 19:50:06 -0700

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 //Where profile information will appear
 const overview = document.querySelector(".overview");
 const username = "allison-conrey";
+const displayReposList = document.querySelector(".repo-list");
 
 const getUserInfo = async function () {
     const userInfo = await fetch(
-        `https://api.github.com/users/${username}`
-    );
+        `https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    console.log(data);
-
     displayUserInfo(data);
   };
 
   getUserInfo();
 
-  const displayUserInfo = function(data) {
+  const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = `
@@ -29,4 +27,25 @@ const getUserInfo = async function () {
     </div> 
     `;
     overview.append(div);
+
+    gitRepos();
+  };
+
+  const gitRepos = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+
+    //console.log(repoData);
+
+    displayRepo(repoData);
+  };
+
+  const displayRepo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+
+        displayReposList.append(repoItem);
+    }
   };
